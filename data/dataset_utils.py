8e55a9ebe71581576b1bfba678f5a5f31a1c4ec0
viswavi <vijayv@andrew.cmu.edu>
2024-03-07 15:26:37 -0500

    Save

--- data/dataset_utils.py
+++ data/dataset_utils.py
@@ -1,11 +1,16 @@
 from datasets import load_dataset
+import jsonlines
 
-from FollowBench.code.rule_based_evaluation import save_evaluate_example_constraint, save_csl_example_constraint
-from FollowBench.code.gpt4_based_evaluation import save_discriminative_evaluation, save_csl_evaluation
+from data.FollowBench.code.rule_based_evaluation import save_evaluate_example_constraint, save_csl_example_constraint
+from data.FollowBench.code.gpt4_based_evaluation import save_discriminative_evaluation, save_csl_evaluation
 
 def load_infobench():
     dataset = load_dataset("kqsong/InFoBench")
     return dataset
 
-def load_followbench(followbench_dir = "FollowBench/data/"):
+def load_ifeval():
+    dataset = list(jsonlines.open("data/ifeval/input_data.jsonl"))
+    return dataset
+
+def load_followbench(followbench_dir = "FollowBench/data"):
     breakpoint()
--- run_req_eval.py
+++ run_req_eval.py
@@ -1,4 +1,4 @@
-from datasets.dataset_utils import load_infobench, load_followbench
+from data.dataset_utils import load_infobench, load_followbench
 
 if __name__ == "__main__":
     breakpoint()
